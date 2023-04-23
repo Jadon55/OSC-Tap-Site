22134e3bd7822ce1401423bd3b01fddc58015650
Jadon55 <78763124+Jadon55@users.noreply.github.com>
2023-04-22 19:15:22 -0700

    Add files via upload

--- OSC Site/app.py
+++ OSC Site/app.py
@@ -6,10 +6,8 @@ import random
 from pythonosc.udp_client import SimpleUDPClient
 from flask import Flask, jsonify, render_template, request
 
-# OSC
+#default values for the lighting board and most common osc commands
 send = False
-hasMore = False
-    #default values for the lighting board and most common osc commands
 target_ip = "10.0.0.196"
 target_port = "8000"
 in_order = True
@@ -18,7 +16,7 @@ end_value = "275"
 osc_command = "/eos/macro/x/fire"
 currentList = []
 
-
+# makes a list of values (in order) to be used in the osc command, list is called "currentList"
 def createList():
     global currentList
     currentList = []                            # clear list
@@ -27,39 +25,42 @@ def createList():
         currentList.append(i+int(start_value))
     return 0
 
+# returns a random element from the "currentList" list, and removes it from the list. If the list is empty, it calles createList()
 def getRandom():
     global currentList
-    if not currentList:
-        createList()
+    if not currentList: # list is empty
+        createList()    # makes list again
         index = random.randint(0, len(currentList) - 1)
         return str(currentList.pop(index))
-    else:
+    else:               # list has at least 1 item
         index = random.randint(0, len(currentList) - 1)
         return str(currentList.pop(index))
 
+# returns the next element from the "currentList" list, and removes it from the list. If the list is empty, it calles createList()
 def getNext():
     global currentList
-    if not currentList:
-        createList()
+    if not currentList: # list is empty
+        createList()    # makes list again
         return str(currentList.pop(0))
-    else:
+    else:               # list has at least 1 item
         return str(currentList.pop(0))
 
+# primary function that runs the OSC loop in a thread
 def runOSC():
-    createList()
-    # print(currentList)
+    createList()    #creaes listt
     while True:
-        if send == True:
-            client = SimpleUDPClient(target_ip, int(target_port))
+        if send == True:    #only sends commands if the button on the home page says "on"
+            client = SimpleUDPClient(target_ip, int(target_port))   # makes a client for the OSC commands to be sent to using the port and ip
             if in_order == False:
                 client.send_message(osc_command.replace("x", getNext()), 1)
             else:
                 client.send_message(osc_command.replace("x", getRandom()), 1)
-            time.sleep(lastAverage)
+            time.sleep(lastAverage) # sleeps for the average time between taps
 
+# creates the thread that will call runOSC() in it
 t2 = threading.Thread(target=runOSC)
 
-#Flask
+# Creates Flask
 app = Flask(__name__)
 
 last_tap_time = None
@@ -70,18 +71,19 @@ lastAverage = 1
 
 
 
-
+# home page
 @app.route('/')
 def index():
     global lastAverage
     return render_template('index.html', lastAverage=lastAverage)
 
+# function called by index.html that returns the most recent recorded average time between taps, used for setting the flash rate of the button
 @app.route('/getTime', methods=['POST'])
 def getTime():
     global lastAverage
     return jsonify({'lastAverage': lastAverage})
 
-
+# function called by index.html when the "tap" button is pressed. Function is responsible for calculating the time between taps
 @app.route('/tap', methods=['POST'])
 def tap():
     global last_tap_time, time_diffs, lastAverage
@@ -91,7 +93,7 @@ def tap():
         time_diff = current_time - last_tap_time
         last_tap_time = current_time
 
-        # shift the array to the right and insert the new time difference
+        # shift the array to the right and insert the new time difference, and removes the "oldest" time from the array
         time_diffs[0:2] = time_diffs[1:3]
         time_diffs[2] = time_diff
 
@@ -108,7 +110,6 @@ def tap():
                 print("     Average time difference: "+ str(lastAverage))
             else:
                 print("     Last Average: "+ str(lastAverage))
-                # print("Time differences too far apart")
         else:
             print("     Not enough time differences")
 
@@ -117,11 +118,12 @@ def tap():
 
     return jsonify({})
 
+# settings page
 @app.route('/settings')
 def settings():
     return render_template('settings.html', target_ip=target_ip, target_port=target_port, in_order=in_order, start_value=start_value, end_value=end_value, osc_command=osc_command)
 
-
+# function called by settings.html to save the values in the form to python
 @app.route('/save-settings', methods=['POST'])
 def save_settings():
     global target_ip, target_port, in_order, start_value, end_value, osc_command
@@ -134,6 +136,7 @@ def save_settings():
     createList()
     return 'OK', 200
 
+# funcion called by settings.html to enable the OSC commands to be sent
 @app.route('/saveOn', methods=['POST'])
 def saveOn():
     global send
@@ -141,6 +144,7 @@ def saveOn():
     print("on")
     return jsonify({})
 
+# funcion called by settings.html to stop the OSC commands from being sent
 @app.route('/saveOff', methods=['POST'])
 def saveOff():
     global send
@@ -149,11 +153,14 @@ def saveOff():
     return jsonify({})
 
 
+# Help page
 @app.route('/help')
 def help():
     return render_template('help.html')
 
 
+
+# Main Function
 if __name__ == '__main__':
-    t2.start()
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    t2.start()  # start the thread that runs the OSC loop
+    app.run(host="0.0.0.0", port=5000, debug=True) # start the flask web app
